00073c77596814b9b21600bf77af7c06df961c5d
bayleighanne <bayhaf718@gmail.com>
2018-06-06 20:33:40 +0000

    automatically backed up by learn

--- amazon_cart.rb
+++ amazon_cart.rb
@@ -1,1 +1,10 @@
 cart_item_prices = [12.43, 19.89, 3.49, 75.00]
+cart_item_prices.push(5.3)
+
+puts cart_item_prices
+
+counts = 1
+cart_item_prices.each do |price|
+  puts "item #{count}: #{price}"
+  count += 1
+end
